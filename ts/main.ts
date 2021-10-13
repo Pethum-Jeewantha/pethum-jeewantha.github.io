e0de70cc1164163e9217180fb86e1cae71b18e74
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-10-13 21:42:37 +0530

    add nav bar and parallax effect to home

--- ts/main.ts
+++ ts/main.ts
@@ -1,20 +1,5 @@
-const texts = [
-    'Welcome',
-    'Coming Soon...😉'
-];
-
-let i = 0;
-let j = 0;
-
-setInterval(()=> {
-
-    $("#text").html(texts[j].substring(0, i+1));
-    if (i === (texts[j].length + 30)){
-        i = 0;
-        j++;
-        if (j === texts.length) j = 0;
-    }
-    i++;
-
-}, 50);
-
+$(window).on('scroll',()=>{
+    let navbar = document.querySelector(".navbar");
+    navbar.classList.toggle("sticky", window.scrollY > +($(window).height() / 2));
+    navbar.classList.toggle("sticky", window.scrollY > +($(window).height() - $('.navbar').height()));
+});
