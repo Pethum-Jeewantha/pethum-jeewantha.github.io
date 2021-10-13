99ab10f6f49a97564e2b244afe7efffabb330909
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-10-13 23:18:50 +0530

    Add about card with some styles

--- ts/main.ts
+++ ts/main.ts
@@ -1,5 +1,4 @@
 $(window).on('scroll',()=>{
     let navbar = document.querySelector(".navbar");
-    navbar.classList.toggle("sticky", window.scrollY > +($(window).height() / 2));
     navbar.classList.toggle("sticky", window.scrollY > +($(window).height() - $('.navbar').height()));
 });
